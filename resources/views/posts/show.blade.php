3235b2b6f56566bdd9cb05f1bcdb4578bad1cbcb
emmepi92 <mariapiacoccioli@live.it>
2021-09-16 15:28:11 +0200

    button go back addes

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container container-button">
+    <button><a href=" {{ route('posts.index') }} ">Tutti i post</a></button>
+  </div>
 
 <div class="container">
     <h3>Autore: {{$post->author}}</h3>
